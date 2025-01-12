6d6d4db3b011e8e6828afd768e1afce14e2ae662
5Stygian <121655781+Blitzy5656@users.noreply.github.com>
2025-01-12 09:41:19 +0000

    comment position changes

--- scripts.js
+++ scripts.js
@@ -16,10 +16,10 @@ function displayRandomNumbers() {
        
         clearInterval(interval);
         if (count >= 25) {               //og value: 25
-            intervalTime *= 1.001;        //og value: += 5
+            intervalTime *= 1.001;       //og value: += 5
             console.log("count >= 35");
             if (count = 35) {   
-                console.log("finished");         //og value: 45
+                console.log("finished"); //og value: 45
                 return;
             }
         } else {
